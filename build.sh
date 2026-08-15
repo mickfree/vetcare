5b994a39ab51965b0479533a89b4ab4400d84495
Michael Romulo Luna Abad <lydoived@gmail.com>
2026-08-14 19:17:05 -0500

    feat: add new command

--- build.sh
+++ build.sh
@@ -12,3 +12,5 @@ python manage.py makemigrations
 python manage.py migrate
 
 python manage.py createsuperuser --noinput
+
+python manage.py shell -c "import os; from django.contrib.auth import get_user_model; User=get_user_model(); User.objects.filter(username=os.environ['DJANGO_SUPERUSER_USERNAME']).update(role='ADMIN', is_staff=True, is_superuser=True, is_active=True)"
